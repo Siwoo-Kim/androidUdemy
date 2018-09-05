5f10b5ec6b3b1016ad5ee2b62f184948a3bfabe8
SiWoo Kim <35586824+Siwoo-Kim@users.noreply.github.com>
2018-09-04 17:11:33 -0700

    first commit

---
+++ app/src/main/java/com/angularudemy/siwoo/myapp/MainActivity.java
@@ -0,0 +1,15 @@
+package com.angularudemy.siwoo.myapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.setContentView(R.layout.activity_main);
+    }
+
+}
